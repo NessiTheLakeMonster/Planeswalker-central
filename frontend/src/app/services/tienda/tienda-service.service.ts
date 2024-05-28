6c4d1fe3b2b4298ed00e8473ebeb34b25e34b278
Inés Mª Barrera Llerena <imbarreral020301@gmail.com>
2024-05-28 09:08:04 +0200

    Gestion de roles en back funcionando y en front se añadio interceptor

--- frontend/src/app/services/tienda/tienda-service.service.ts
+++ frontend/src/app/services/tienda/tienda-service.service.ts
@@ -22,7 +22,7 @@ export class TiendaServiceService {
   postArticulo(body: VenderCarta): Observable<HttpResponse<ArticuloTienda>> {
     return this.http.post<ArticuloTienda>(env.URL + 'api/tienda', body, {
       observe: 'response' as 'response',
-      params: { auth: 1 }
+      params: { auth: 2 }
     })
   }
 }
