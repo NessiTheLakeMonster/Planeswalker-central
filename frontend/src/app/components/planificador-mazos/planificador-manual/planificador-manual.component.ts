c9945b6e26a6695a18dcd011d49292b6c7f727e7
Inés Mª Barrera Llerena <imbarreral020301@gmail.com>
2024-05-29 20:42:22 +0200

    Cartas se guardan en un array de mazo

--- frontend/src/app/components/planificador-mazos/planificador-manual/planificador-manual.component.ts
+++ frontend/src/app/components/planificador-mazos/planificador-manual/planificador-manual.component.ts
@@ -1,24 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CartasServiceService } from '../../../services/cartas/cartas-service.service';
 import { CartaBuscar } from '../../../interfaces/cartas-interface';
 import { HttpResponse } from '@angular/common/http';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgbAlertModule, NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-planificador-manual',
   standalone: true,
   imports: [
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgbPopoverModule,
+    NgbAlertModule
   ],
   templateUrl: './planificador-manual.component.html',
   styleUrl: './planificador-manual.component.css'
 })
-export class PlanificadorManualComponent {
+export class PlanificadorManualComponent implements OnInit {
   loading: boolean = false;
 
+  tipoAlerta: string = '';
+  mensajeAlerta: string = '';
+  mostrarAlerta: boolean = false;
+
   cartas: any = [];
+  mazo: any = [];
 
   buscar = new FormGroup({
     name: new FormControl('', Validators.required)
@@ -28,6 +36,15 @@ export class PlanificadorManualComponent {
     private router: Router,
     private cartasService: CartasServiceService
   ) { }
+
+  ngOnInit() {
+    this.tipoAlerta = '';
+    this.mensajeAlerta = '';
+  }
+
+  cerrarAlerta() {
+    this.mostrarAlerta = false;
+  }
 
   btnRecomendador() {
     this.router.navigate(['/recomendador']);
@@ -52,8 +69,22 @@ export class PlanificadorManualComponent {
     });
   }
 
-  elegirCarta(id : string) {
-    console.log(id);
+  elegirCarta(id: string) {
+    const carta = this.cartas.find((carta: any) => carta.id === id);
+    if (carta) {
+      this.mazo.push(carta);
+
+      console.log(this.mazo);
+
+      this.mostrarAlerta = true;
+      this.tipoAlerta = 'success';
+      this.mensajeAlerta = 'Carta añadida al mazo';
+
+      setTimeout(() =>
+        this.mostrarAlerta = false,
+        3000
+      );
+    }
   }
 
 }
